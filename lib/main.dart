5bdfcc356a0408ac59ae0e40c1565ff238c7a670
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-10-08 13:58:13 +0530

    Integrate Riverpod for state management and update dependencies in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +13,7 @@ void main() async {
 
     appleProvider: AppleProvider.appAttest,
   );
-  runApp(const MainApp());
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
